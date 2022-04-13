26af5e5f8b34f1fb06fbe7f9dba263fe98f0d6bc
nagineniv <venkatesh.nagineni@verivox.com>
2022-04-13 17:41:57 +0200

    test

--- dist/index.js
+++ dist/index.js
@@ -5,11 +5,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const HttpClient_1 = require("typed-rest-client/HttpClient");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-    res.send('Express + TypeScript Server');
+    res.writeHead(302, 'https://git.com');
+});
+app.get('/test', (req, res) => {
+    new HttpClient_1.HttpClient('').get('http://localhost:3000').then((data) => {
+        res.send(data.message.headers);
+    });
 });
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
--- index.ts
+++ index.ts
@@ -1,13 +1,19 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-
+import { HttpClient } from 'typed-rest-client/HttpClient';
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+  res.writeHead(302, 'https://git.com');
+});
+
+app.get('/test', (req: Request, res: Response) => {
+  new HttpClient('').get('http://localhost:3000').then((data) => {
+    res.send(data.message.headers)
+  })
 });
 
 app.listen(port, () => {
